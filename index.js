bc3156f2bbb97140a4b24eb73d608094cf1f6d61
tomcoakes <tomcoakes@gmail.com>
2020-07-30 15:17:48 +0100

    add collision detection and randomise ball colour when colliding

--- index.js
+++ index.js
@@ -4,22 +4,46 @@ const ctx = canvas.getContext("2d")
 let x = canvas.width / 2
 let y = canvas.height - 30
 
-const dx = 2
-const dy = -2
+let dx = 2
+let dy = -2
+
+const ballRadius = 10
+let ballColour = "#0095DD"
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
+}
 
 function drawBall() {
   ctx.beginPath()
-  ctx.arc(x, y, 10, 0, Math.PI * 2)
-  ctx.fillStyle = "#0095DD"
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
+  ctx.fillStyle = ballColour
   ctx.fill()
   ctx.closePath()
+}
+
+function randomiseBallColour() {
+  ballColour = `rgb(${getRandomInt(0, 256)}, ${getRandomInt(0, 256)}, ${getRandomInt(0, 256)})`
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawBall()
+
   x += dx
   y += dy
+
+  if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+    dx = -dx
+    randomiseBallColour()
+  }
+
+  if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+    dy = -dy
+    randomiseBallColour()
+  }
 }
 
 setInterval(draw, 10)
